b1563a5af3f8c8b28a5cb93b65427ed89e46286a
Kashyap Kaki <kashyap3111992@gmail.com>
2021-12-16 08:51:54 +0530

    url changes for testing

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  USER: "kashyap3111992@gmail.com", //'365Physio123@gmail.com', //Please add your domain email
+  USER: "contact@365physiotherapy.ie", //'365Physio123@gmail.com', //Please add your domain email
   PASS: "Admin1365physio#", //'365Physio@Therapy123'
 };
--- endpoints/productRoutes.js
+++ endpoints/productRoutes.js
@@ -1,42 +1,41 @@
-const mongoose = require('mongoose');
-const Product = mongoose.model('products');
-const nodemailer = require('nodemailer');
-const creds = require('../config');
+const mongoose = require("mongoose");
+const Product = mongoose.model("products");
+const nodemailer = require("nodemailer");
+const creds = require("../config");
 
 module.exports = (app) => {
   const transport = {
-    host: "smtpout.secureserver.net",  
+    host: "smtpout.secureserver.net",
     secure: true,
     secureConnection: false, // TLS requires secureConnection to be false
     tls: {
-        ciphers:'SSLv3'
+      ciphers: "SSLv3",
     },
-    requireTLS:true,
+    requireTLS: true,
     port: 465,
     auth: {
-        user: creds.USER,
-        pass: creds.PASS
-    }
-  }
-  
+      user: creds.USER,
+      pass: creds.PASS,
+    },
+  };
+
   const transporter = nodemailer.createTransport(transport);
-  
-  
+
   transporter.verify((error, success) => {
     if (error) {
       console.log(error);
     } else {
-      console.log('Server is ready to take messages');
+      console.log("Server is ready to take messages");
     }
   });
-  
+
   app.get(`/api/product/:id`, async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     let product = await Product.findById(id, req.body);
     return res.status(202).send({
-        error: false,
-        product
-    })
+      error: false,
+      product,
+    });
   });
 
   app.get(`/api/product`, async (req, res) => {
@@ -48,66 +47,64 @@ module.exports = (app) => {
     let product = await Product.create(req.body);
     return res.status(201).send({
       error: false,
-      product
-    })
-  })
+      product,
+    });
+  });
 
   app.put(`/api/product/:id`, async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
 
     let product = await Product.findByIdAndUpdate(id, req.body);
 
     return res.status(202).send({
       error: false,
-      product
-    })
-
+      product,
+    });
   });
 
   app.delete(`/api/product/:id`, async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
 
     let product = await Product.findByIdAndDelete(id);
 
     return res.status(202).send({
       error: false,
-      product
-    })
-
+      product,
+    });
   });
 
-  app.post('/api/send', (req, res, next) => {
+  app.post("/api/send", (req, res, next) => {
     console.log("API SEND");
-    let name = req.body['first-name'] + ' ' + req.body['last-name']
+    let name = req.body["first-name"] + " " + req.body["last-name"];
     let contact = req.body.number;
-    let email = req.body.email
-    let service = req.body.dropdown
-    let message = req.body.message
-    let content = `name: ${name} \n contact: ${contact} \n email: ${email} \n message: ${message} \n service: ${service}`
-  
+    let email = req.body.email;
+    let service = req.body.dropdown;
+    let message = req.body.message;
+    let content = `name: ${name} \n contact: ${contact} \n email: ${email} \n message: ${message} \n service: ${service}`;
+
     let mail = {
       from: req.body.email,
-      to: creds.USER,
-      subject: 'New Message from Contact Form',
-      text: content
-    }
-  
+      to: "kashyap3111992@gmail.com", //creds.USER,
+      subject: "New Message from Contact Form",
+      text: content,
+    };
+
     transporter.sendMail(mail, (err, data) => {
       if (err) {
-        console.log("send mail as error"+err+data);
+        console.log("send mail as error" + err + data);
         res.json({
-          status: 'fail',
-          data:err
+          status: "fail",
+          data: err,
         });
         res.end();
       } else {
-        console.log("send mail "+data);
+        console.log("send mail " + data);
         res.json({
-         status: 'success',
-         data: mail
-        });res.end();
+          status: "success",
+          data: mail,
+        });
+        res.end();
       }
-    })
+    });
   });
-
-}
+};
